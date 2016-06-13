6b2326d01a99a7bd1cadfc59b804f4e66e995761
niklasben <nachtmahr2010@googlemail.com>
2016-06-13 16:00:24 +0200

    Narrowed down Example Samples

--- other_scripts/deu_keywords.py
+++ other_scripts/deu_keywords.py
@@ -39,105 +39,109 @@ from nltk import pos_tag, word_tokenize
 from nltk.corpus import stopwords, PlaintextCorpusReader
 from nltk.tokenize import wordpunct_tokenize
 from nltk.tag import PerceptronTagger
-from pattern.de import parse, split
+# from pattern.de import parse, split
+import treetaggerwrapper as ttw
 import shutil
 
 for dirpath, dirs, files in os.walk('../DDCs/deu'):
-    for filename in fnmatch.filter(files, '*.xml'):
+    # print dirpath[-3:]
+    if dirpath[-3:] == '710' or dirpath[-3:] == '330':
+        print dirpath
+    # for filename in fnmatch.filter(files, '*.xml'):
 
-        foldername = '../temp/temp_' + dirpath[-3:] + '/'
-        newfilename = filename[11:-4] + '.txt'
+        # foldername = '../temp/temp_' + dirpath[-3:] + '/'
+        # newfilename = filename[11:-4] + '.txt'
 
-        with open(dirpath + '/' + filename, 'r') as openfile:
-            s = mmap.mmap(openfile.fileno(), 0, access=mmap.ACCESS_READ)
-            if s.find('<dc:description xml:lang="deu">') != -1:
-                line = openfile.read()
-                start = line.index('<dc:description xml:lang="deu">') + \
-                    len('<dc:description xml:lang="deu">')
-                end = line.index('</dc:description>', start)
+        # with open(dirpath + '/' + filename, 'r') as openfile:
+        #     s = mmap.mmap(openfile.fileno(), 0, access=mmap.ACCESS_READ)
+        #     if s.find('<dc:description xml:lang="deu">') != -1:
+        #         line = openfile.read()
+        #         start = line.index('<dc:description xml:lang="deu">') + \
+        #             len('<dc:description xml:lang="deu">')
+        #         end = line.index('</dc:description>', start)
 
-                foldername = '../temp/temp_' + dirpath[-3:] + '/'
+                # foldername = '../temp/temp_' + dirpath[-3:] + '/'
 
-                if not os.path.exists(os.path.dirname(foldername)):
-                    try:
-                        os.makedirs(os.path.dirname(foldername))
-                    except OSError as exc:  # Guard against race condition
-                        if exc.errno != errno.EEXIST:
-                            raise
+                # if not os.path.exists(os.path.dirname(foldername)):
+                #     try:
+                #         os.makedirs(os.path.dirname(foldername))
+                #     except OSError as exc:  # Guard against race condition
+                #         if exc.errno != errno.EEXIST:
+                #             raise
 
-            newfilename = filename[11:-4] + '.txt'
-            with open(foldername + newfilename, 'w') as newfile:
-                newfile.write(line[start:end])
-
-
-foldername2 = '../temp/desc/'
-
-if not os.path.exists(os.path.dirname(foldername2)):
-    try:
-        os.makedirs(os.path.dirname(foldername2))
-    except OSError as exc:  # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
+            # newfilename = filename[11:-4] + '.txt'
+            # with open(foldername + newfilename, 'w') as newfile:
+            #     newfile.write(line[start:end])
 
 
-for dirpath, dirs, files in os.walk('../temp'):
-    with open('../temp/desc/desc_' + dirpath[-3:] + '.txt', 'w') as writefile:
-        for filename in fnmatch.filter(files, '*.txt'):
-            with open(dirpath + '/' + filename, 'r') as openfile:
-                line = openfile.read()
-            writefile.write(line + '\n\n')
+# foldername2 = '../temp/desc/'
+
+# if not os.path.exists(os.path.dirname(foldername2)):
+#     try:
+#         os.makedirs(os.path.dirname(foldername2))
+#     except OSError as exc:  # Guard against race condition
+#         if exc.errno != errno.EEXIST:
+#             raise
 
 
-replacements_umlaute = {
-                    '\\xc3\\xa4': 'ä',
-                    '\\xc3\\xbc': 'ü',
-                    '\\xc3\\xb6': 'ö',
-                    '\\xc3\\x9f': 'ß'
-                    }
+# for dirpath, dirs, files in os.walk('../temp'):
+#     with open('../temp/desc/desc_' + dirpath[-3:] + '.txt', 'w') as writefile:
+#         for filename in fnmatch.filter(files, '*.txt'):
+#             with open(dirpath + '/' + filename, 'r') as openfile:
+#                 line = openfile.read()
+#             writefile.write(line + '\n\n')
 
-foldername3 = '../temp/desc_umlaute/'
 
-if not os.path.exists(os.path.dirname(foldername3)):
-    try:
-        os.makedirs(os.path.dirname(foldername3))
-    except OSError as exc:  # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
+# replacements_umlaute = {
+#                     '\\xc3\\xa4': 'ä',
+#                     '\\xc3\\xbc': 'ü',
+#                     '\\xc3\\xb6': 'ö',
+#                     '\\xc3\\x9f': 'ß'
+#                     }
 
-for dirpath, dirs, files in os.walk('../temp/desc'):
-    for filename in fnmatch.filter(files, '*.txt'):
-        with open(dirpath + '/' + filename, 'r') as originalfile, \
-         open('../temp/desc_umlaute/' + filename, 'w') as writefile:
-            for line in originalfile:
-                line = line.strip()
-                for src, target in replacements_umlaute.iteritems():
-                    line = line.replace(src, target)
-                writefile.write(line)
+# foldername3 = '../temp/desc_umlaute/'
+
+# if not os.path.exists(os.path.dirname(foldername3)):
+#     try:
+#         os.makedirs(os.path.dirname(foldername3))
+#     except OSError as exc:  # Guard against race condition
+#         if exc.errno != errno.EEXIST:
+#             raise
+
+# for dirpath, dirs, files in os.walk('../temp/desc'):
+#     for filename in fnmatch.filter(files, '*.txt'):
+#         with open(dirpath + '/' + filename, 'r') as originalfile, \
+#          open('../temp/desc_umlaute/' + filename, 'w') as writefile:
+#             for line in originalfile:
+#                 line = line.strip()
+#                 for src, target in replacements_umlaute.iteritems():
+#                     line = line.replace(src, target)
+#                 writefile.write(line)
 
 
 # stopword_list = stopwords.words('german')
 # stopword_list.extend(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
 # stopword_list = set(stopword_list)
 
-foldername4 = '../temp/desc_pos/'
+# foldername4 = '../temp/desc_pos/'
 
-if not os.path.exists(os.path.dirname(foldername4)):
-    try:
-        os.makedirs(os.path.dirname(foldername4))
-    except OSError as exc:  # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
+# if not os.path.exists(os.path.dirname(foldername4)):
+#     try:
+#         os.makedirs(os.path.dirname(foldername4))
+#     except OSError as exc:  # Guard against race condition
+#         if exc.errno != errno.EEXIST:
+#             raise
 
-for dirpath, dirs, files in os.walk('../temp/desc_umlaute'):
-    for filename in fnmatch.filter(files, '*.txt'):
-        with open(dirpath + '/' + filename, 'r') as originalfile, \
-         open('../temp/desc_pos/' + filename, 'w') as writefile:
-            for line in originalfile:
-                s = parse(line)
-                for sentence in split(s):
-                    sentence = str(sentence)
-                    sentence = sentence.lstrip('Sentence(\'').rstrip('\')')
-                    writefile.write(sentence + '\n\n')
+# for dirpath, dirs, files in os.walk('../temp/desc_umlaute'):
+#     for filename in fnmatch.filter(files, '*.txt'):
+#         with open(dirpath + '/' + filename, 'r') as originalfile, \
+#          open('../temp/desc_pos/' + filename, 'w') as writefile:
+#             for line in originalfile:
+#                 s = parse(line)
+#                 for sentence in split(s):
+#                     sentence = str(sentence)
+#                     sentence = sentence.lstrip('Sentence(\'').rstrip('\')')
+#                     writefile.write(sentence + '\n\n')
 
 
 # shutil.rmtree('../temp')
