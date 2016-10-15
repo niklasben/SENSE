8ea9979f88c957bf417c57ded8b216660294c264
niklasben <nachtmahr2010@googlemail.com>
2016-10-16 00:21:11 +0200

    New Django Test

--- djangotest/testsite/apps.py
+++ djangotest/testsite/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
-    name = 'blog'
+class TestsiteConfig(AppConfig):
+    name = 'testsite'
