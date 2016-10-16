9432db2d4baef58a2b34ac2b33cfcd15a0e46b4b
niklasben <nachtmahr2010@googlemail.com>
2016-10-16 21:53:03 +0200

    Djangotest

--- djangotest/testsite/urls.py
+++ djangotest/testsite/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^contact/$', views.contact, name='contact'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- djangotest/testsite/views.py
+++ djangotest/testsite/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
+from django.forms import ModelForm
 
 
 # Create your views here.
 def index(request):
     """Hello World."""
     return render_to_response('index.html')
+
+
+def contact(request):
+    """Contact Form."""
+    form_class = ModelForm
+
+    return render(request, 'contact.html', {
+        'form': form_class,
+    })
