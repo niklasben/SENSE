02f3cafa5cd3895d229deeaf42050a3feb724c4f
niklasben <nachtmahr2010@googlemail.com>
2016-10-16 19:42:02 +0200

    Django Test

---
+++ djangotest/testsite/templates/form.py
@@ -0,0 +1,15 @@
+
+
+
+<div class="form-group">
+    <label for="comment">Comment:</label>
+    <form action="#" method="post" enctype="text/plain" id="text_form">
+        <textarea class="form-control" name="text_textarea" rows="10" cols="100" placeholder="Insert text here" required="yes" autofocus="yes"></textarea>
+        <span class="help-block">
+            Bitte geben Sie den zu untersuchenden Text in das obige Textfeld ein.
+        </span>
+        <!-- <br /> -->
+        <button type="reset" class="btn btn-warning">Eingaben zurücksetzen</button>
+        <button type="submit" class="btn btn-default">Eingaben absenden</button>
+    </form>
+</div>
