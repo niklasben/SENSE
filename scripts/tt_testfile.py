03223c65c8fcf20a40c6767cc2faf747b6eae984
niklasben <nachtmahr2010@googlemail.com>
2016-10-20 14:41:09 +0200

    TreeTagger Tests

--- scripts/tt_testfile.py
+++ scripts/tt_testfile.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from treetagger import TreeTagger
-from pprint import pprint
+import treetaggerwrapper
+import pprint
+# from treetagger.TreeTagger import TreeTagger
+# from pprint.pprint import pprint
+#
+# tt = TreeTagger(language='english')
+# pprint(tt.tag('What is the airspeed of an unladen swallow?'))
 
-tt = TreeTagger(language='english')
-pprint(tt.tag('What is the airspeed of an unladen swallow?'))
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='de')
+tags = tagger.tag_text("This is a very short text to tag.")
+pprint.pprint(tags)
