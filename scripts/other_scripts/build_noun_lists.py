8b44c7e13f6e1baf5046d35f5035ed5e3dd22f20
niklasben <nachtmahr2010@googlemail.com>
2016-11-07 15:32:23 +0100

    Added JSON Files

--- scripts/other_scripts/build_noun_lists.py
+++ scripts/other_scripts/build_noun_lists.py
@@ -1,21 +1,34 @@
-# -*- coding: utf
+# -*- coding: utf-8 -*-
 """Docstring."""
 
 import re
 import os
 import fnmatch
+import json
 from pattern.de import parse, split, pprint, tag
-from pprint import pprint
+import sys
+
+
+# Set default encoding to UTF-8
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def buildWordList():
-    """Docstring."""
+    """
+    Function to build lists and dictionaries.
 
+    Function exports txt-files incl. dictionaries and lists from both DDC for
+    the words that are tagged as NE and NN.
+    """
+
+    # Defining list variables
     list330NE = []
     list330NN = []
     list710NE = []
     list710NN = []
 
+    # Defining dictionary variables
     dict330NE = {}
     dict330NN = {}
     dict710NE = {}
@@ -32,41 +45,63 @@ def buildWordList():
                 ddcFromFilepath = dirpath[-3:]
 
                 for word, postag in tag(parsefile, tagset="STTS"):
+                    word = word.decode('utf-8')
                     if ddcFromFilepath == '330':
-                        print ddcFromFilepath + '\t' + word
-                    # if postag == 'NN':
-                    #     if word not in dictNN.keys():
-                    #         dictNN[word] = 1
-                    #     elif word in dictNN.keys():
-                    #         dictNN[word] += 1
-                    #     else:
-                    #         pass
-                    # elif postag == 'NE':
-                    #     if word not in dictNE.keys():
-                    #         dictNE[word] = 1
-                    #     elif word in dictNE.keys():
-                    #         dictNE[word] += 1
-                    #     else:
-                    #         pass
-                    # else:
-                    #     pass
+                        # print ddcFromFilepath + '\t' + word
+                        if postag == 'NN':
+                            list330NN.append(word)
+                            if word not in dict330NN.keys():
+                                dict330NN[word] = 1
+                            elif word in dict330NN.keys():
+                                dict330NN[word] += 1
+                            else:
+                                pass
+                        elif postag == 'NE':
+                            list330NE.append(word)
+                            if word not in dict330NE.keys():
+                                dict330NE[word] = 1
+                            elif word in dict330NE.keys():
+                                dict330NE[word] += 1
+                            else:
+                                pass
+                        else:
+                            pass
+                    elif ddcFromFilepath == '710':
+                        # print ddcFromFilepath + '\t' + word
+                        if postag == 'NN':
+                            list710NN.append(word)
+                            if word not in dict710NN.keys():
+                                dict710NN[word] = 1
+                            elif word in dict710NN.keys():
+                                dict710NN[word] += 1
+                            else:
+                                pass
+                        elif postag == 'NE':
+                            list710NE.append(word)
+                            if word not in dict710NE.keys():
+                                dict710NE[word] = 1
+                            elif word in dict710NE.keys():
+                                dict710NE[word] += 1
+                            else:
+                                pass
+                        else:
+                            pass
+
+    with open('../../collecting/dict330NE.txt', 'w') as exportfile330NE:
+        json.dump(dict330NE, exportfile330NE, sort_keys=True, indent=4,
+                  separators=(',', ': '))
+
+    with open('../../collecting/dict330NN.txt', 'w') as exportfile330NN:
+        json.dump(dict330NN, exportfile330NN, sort_keys=True, indent=4,
+                  separators=(',', ': '))
+
+    with open('../../collecting/dict710NE.txt', 'w') as exportfile710NE:
+        json.dump(dict710NE, exportfile710NE, sort_keys=True, indent=4,
+                  separators=(',', ': '))
+
+    with open('../../collecting/dict710NN.txt', 'w') as exportfile710NN:
+        json.dump(dict710NN, exportfile710NN, sort_keys=True, indent=4,
+                  separators=(',', ': '))
 
 
-
-
-
-
-
-
-
-
-                # ddcFromFilepath = dirpath[-3:]
-                # if ddcFromFilepath == '330':
-                #     pass
-                # elif ddcFromFilepath == '710':
-                #     pass
-                # else:
-                #     print 'Irgendeine DDC die nicht da sein dürfte'
-
-
-buildWordList()
+buildWordList()  # Call function buildWordList
